b5881574df8d8f45afd2c36c1dea352a8431bea5
Imre Mocsari <imremocsari1@yahoo.co.uk>
2016-11-08 16:48:37 +0000

    rails wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+10.times do
+   post = Article.new(
+    title: Faker::Name.title,
+    content: Faker::Lorem,
+   )
+   post.save
+ end
+#
+#
+# 100.times do
+#   post = Post.new(
+#     name: Faker::Commerce.product_name,
+#     url: Faker::Internet.url,
+#     votes: (0..1000).to_a.sample
+#   )
+#   post.save
+# end
